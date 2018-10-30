c983816a6ac3a047c60094b3371785f3aed18326
yveh <abczyh@126.com>
2018-10-31 01:34:46 +0800

    quadraticResidue

--- ZYH/quadraticResidue.cpp
+++ ZYH/quadraticResidue.cpp
@@ -13,18 +13,21 @@ ll pow(ll a, ll x, ll p)
 	}
 	return ret;
 }
-//p is prime && p != 2
-ll solve(ll a, ll p)
+ll inv(ll a, ll p)
+{
+	return pow(a, p - 2, p);
+}
+ll quadraticResidue(ll a, ll p)
 {
 	if (pow(a, (p - 1) / 2, p) == p - 1)
 		return -1;
 	ll s = p - 1;
-	ll t = 0;
+	int t = 0;
 	for (; ~s & 1; s >>= 1)
 		t++;
 	ll b = 1;
 	for (; pow(b, (p - 1) / 2, p) % p == 1; b++);
-	ll inva = pow(a, p - 2, p);
+	ll inva = inv(a, p);
 	
 	ll x = pow(a, (s + 1) / 2, p);  
 	ll e = pow(a, s, p);
@@ -35,28 +38,4 @@ ll solve(ll a, ll p)
 		e = inva * x % p * x % p;
 	}
 	return x;
-}
-int T;
-ll a, p, ans;
-int main()
-{
-	scanf("%d", &T);
-	while (T--)
-	{
-		scanf("%lld%lld", &a, &p);
-		a %= p;
-		if (p == 2)
-		{
-			puts("1");
-			continue;
-		}
-		ans = solve(a, p);
-		if (ans == - 1)
-			puts("No root");
-		else if (ans < p - ans)
-			printf("%lld %lld\n", ans, p - ans);
-		else
-			printf("%lld %lld\n", p - ans, ans);
-	}
-	return 0;
 }
